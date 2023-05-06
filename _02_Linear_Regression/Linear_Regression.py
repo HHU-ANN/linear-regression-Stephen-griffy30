a2c18a1b0a4168b8af761be9ef7a41b2b3698df9
Stephen-griffy30 <132270587+Stephen-griffy30@users.noreply.github.com>
2023-05-06 13:23:32 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -9,38 +9,29 @@ except ImportError as e:
     import numpy as np
 
 def ridge(data):
-   # 假设 'data' 是一个 n x m 的 numpy 数组
-    X = data[:, :-1]
-    y = data[:, -1]
+    # 假设 'data' 是一个 n x m 的 numpy 数组
+    X = data[:-1]
+    y = data[-1]
     XtX = np.dot(X.T, X)
-    XtXpI = XtX + np.eye(X.shape[1])
-    XtXpI_inv = np.linalg.inv(XtXpI)
-    XtY = np.dot(X.T, y)
-    w = np.dot(XtXpI_inv, XtY)
+    lambd = 0.1
+    I = np.eye(XtX.shape[0])
+    w = np.dot(np.linalg.inv(XtX + lambd*I), np.dot(X.T, y))
     return w
     
-def lasso(data, alpha=1.0, max_iter=1000, tol=1e-4):
+def lasso(data):
     # 假设 'data' 是一个 n x m 的 numpy 数组
-    X = data[:, :-1]
-    y = data[:, -1]
-    w = np.zeros((X.shape[1], 1))
-    converged = False
-    iter_count = 0
-    while not converged and iter_count < max_iter:
-        w_old = w.copy()
-        for j in range(X.shape[1]):
-            a_j = 2 * np.sum(X[:, j]**2)
-            c_j = 2 * np.dot(X[:, j], y - np.dot(X, w) + w[j]*X[:, j])
-            if c_j < -alpha:
-                w[j] = (c_j + alpha) / a_j
-            elif c_j > alpha:
-                w[j] = (c_j - alpha) / a_j
-            else:
-                w[j] = 0
-        if np.linalg.norm(w - w_old) < tol:
-            converged = True
-        iter_count += 1
-    return w
+    X = data.reshape((-1, 6))[:, :-1]
+    y = data.reshape((-1, 6))[:, -1]
+    n, m = X.shape
+    # 实现 Lasso 回归
+    w = np.zeros(m)
+    alpha = 0.1
+    n_iterations = 1000
+    for iteration in range(n_iterations):
+        y_pred = X.dot(w)
+        gradients = 2 / n * X.T.dot(y_pred - y) + alpha * np.sign(w)
+        w = w - 0.01 * gradients
+    return X.dot(w)
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
