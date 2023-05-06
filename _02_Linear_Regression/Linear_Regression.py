3eeab2eafc28973f22cd0ef60ac6ebaa20b901eb
Stephen-griffy30 <132270587+Stephen-griffy30@users.noreply.github.com>
2023-05-06 13:33:18 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -1,39 +1,43 @@
-# 最终在main函数中传入一个维度为6的numpy数组，输出预测值
-
 import os
 
 try:
     import numpy as np
+    from scipy.linalg import inv
+    from scipy.optimize import minimize_scalar
 except ImportError as e:
-    os.system("sudo pip3 install numpy")
+    os.system("sudo pip3 install numpy scipy")
     import numpy as np
+    from scipy.linalg import inv
+    from scipy.optimize import minimize_scalar
 
-def ridge(data):
-    # 假设 'data' 是一个 n x m 的 numpy 数组
-    X = data[:-1]
-    y = data[-1]
-    XtX = np.dot(X.T, X)
-    lambd = 0.1
-    I = np.eye(XtX.shape[0])
-    w = np.dot(np.linalg.inv(XtX + lambd*I), np.dot(X.T, y))
-    return w
-    
-def lasso(data):
-    # 假设 'data' 是一个 n x m 的 numpy 数组
-    X = data.reshape((-1, 6))[:, :-1]
-    y = data.reshape((-1, 6))[:, -1]
-    n, m = X.shape
-    # 实现 Lasso 回归
-    w = np.zeros(m)
-    alpha = 0.1
-    n_iterations = 1000
-    for iteration in range(n_iterations):
-        y_pred = X.dot(w)
-        gradients = 2 / n * X.T.dot(y_pred - y) + alpha * np.sign(w)
-        w = w - 0.01 * gradients
-    return X.dot(w)
+def ridge(data, alpha):
+    x, y = data[:, :-1], data[:, -1]
+    n, p = x.shape
+    xtx = np.dot(x.T, x)
+    ridge_term = alpha * np.identity(p)
+    ridge_term[0, 0] = 0 # Don't regularize the intercept
+    xty = np.dot(x.T, y)
+    beta = np.dot(inv(xtx + ridge_term), xty)
+    return np.dot(data[:, :-1], beta)
+
+def lasso(data, alpha):
+    x, y = data[:, :-1], data[:, -1]
+    n, p = x.shape
+    xtx = np.dot(x.T, x)
+    xty = np.dot(x.T, y)
+
+    def objective(beta):
+        return np.sum((y - np.dot(x, beta))**2) + alpha*np.sum(np.abs(beta))
+
+    beta_init = np.zeros(p)
+    beta_init[0] = np.mean(y)  # Intercept is just the mean of y
+    res = minimize_scalar(objective)
+    beta_hat = res.x
+    return np.dot(data[:, :-1], beta_hat)
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
     y = np.load(path + 'y_train.npy')
-    return x, y
+    data = np.hstack((x, y.reshape(-1, 1)))
+    return data
+
