0106cc64aadee50653be452797b2461970996fee
Stephen-griffy30 <132270587+Stephen-griffy30@users.noreply.github.com>
2023-05-06 16:15:18 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -1,5 +1,3 @@
-# 最终在main函数中传入一个维度为6的numpy数组，输出预测值
-
 import os
 
 try:
@@ -12,15 +10,16 @@ except ImportError as e:
 def ridge(data):
     x, y = read_data()
     w = np.dot(np.linalg.inv(np.dot(x.T, x) + 0.5 * np.eye(6)), np.dot(x.T, y))
-    return w @ data
+    w = np.append(w, np.array([1]))  # 添加常数项
+    return np.dot(w, np.append(data, 1))
 
 
 def lasso(data):
     x, y = read_data()
     w = np.array([1, 1, 1, 1, 1, 1])
-
     w = np.dot(np.linalg.inv(np.dot(x.T, x)), np.dot(x.T, y)-0.5*w)
-    return w @ data
+    w = np.append(w, np.array([1]))  # 添加常数项
+    return np.dot(w, np.append(data, 1))
 
 
 def read_data(path='./data/exp02/'):
